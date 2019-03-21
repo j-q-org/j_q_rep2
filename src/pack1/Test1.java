c18f63f04f162e36ec23b04cbc8f2acd18ae4b6c
jackliuhao <jackliuhao@123>
2019-03-21 11:53:19 +0800

    jjjjuu

--- src/pack1/Test1.java
+++ src/pack1/Test1.java
@@ -5,16 +5,12 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		try {
-			//1 加载驱动
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			//2 获得连接
 			String url = "jdbc:oracle:thin:@127.0.0.1:1521:ORCL";
-			  // 连接到127.0.0.1 以1521端口连上oracle，访问其中orcl数据库
 			Connection cn = DriverManager.getConnection(url, "scott", "tiger");
 			System.out.println(cn);
 			
-			//所有数据库连接步骤都是相同的，驱动类名与连接url的写法不同
-			//3 数据库操作.......123123123
+			System.out.println("123123");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
